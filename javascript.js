ea7650c8999c112cfb0fcc4b3f64244e2e6d3a45
Ya Ting Yang <31138824+yy51964@users.noreply.github.com>
2017-11-17 15:37:13 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -54,10 +54,3 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
-
-
-    $(document).ready(function () {
-      $('input[type=submit]').click(function () {
-        $('input[type=submit]').toggleClass('red');
-      });
-    });
